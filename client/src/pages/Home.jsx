3aa9a65b59dbb0799ea43d45ac1d30d108fa7b81
denisaneata93 <denisaneata93@gmail.com>
2025-12-22 20:53:30 +0200

    stilizat homepage
    am facut pagini de css pt homegage, pagina de profesor si pagina de student si am stilizat homepage-ul

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import {Link} from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
     return(
-        <div>
-            <h1>Feedback Continuu</h1>
+        <div className="home-container">
+            <h1 className="home-title">Feedback Continuu</h1>
 
-            <div>
+            <h3 className="home-text">Alege rolul pentru a continua</h3>
+
+            <div className="home-buttons">
                 <Link to="/profesor">
-                <button>Profesor</button>
+                <button className="button">Profesor</button>
                 </Link>
 
                 <Link to="/student">
-                <button>Student</button>
+                <button className="button">Student</button>
                 </Link>
             </div>
 
