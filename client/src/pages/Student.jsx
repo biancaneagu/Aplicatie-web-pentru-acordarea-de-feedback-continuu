f72b692238269f6a15c63f624a84d4f9caa5cfe8
denisaneata93 <denisaneata93@gmail.com>
2025-12-27 23:30:22 +0200

    am facut pagina de student
    am facut partea unde studentul trebuie sa introduca un cod pt a se conecta la o activitate

--- client/src/pages/Student.jsx
+++ client/src/pages/Student.jsx
@@ -1,5 +1,32 @@
+import {useState} from 'react';
+import "./Student.css";
+
 export default function Student() {
+    const [code, setCode] = useState("");
+
+    const handleJoin=() => {
+        if(!code){
+            alert("Introdu un cod valid!");
+            return;
+        }
+
+        alert("Ai introdus codul: " + code);
+    };
+
     return(
-        <h1>Pagina student</h1>
-    )
+        <div className="studentContainer">
+            <h1>Intra in activitate</h1>
+
+            <p>Introdu codul primit de la profesor</p>
+
+            <input className="studentInput"
+             type="text"
+             value={code}
+             onChange={(e)=>setCode(e.target.value)}
+             placeholder="Cod activitate"
+             />
+
+            <button className="studentButton" onClick={handleJoin}>Conecteaza-te</button>
+           </div>
+    );
 }
