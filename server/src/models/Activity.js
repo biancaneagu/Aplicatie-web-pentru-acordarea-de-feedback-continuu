37ae4dd8226661ae22912a909e3ed935b7dcdd45
denisaneata93 <denisaneata93@gmail.com>
2026-01-12 22:27:33 +0200

    am adaugat comentarii si am formatat codul

--- server/src/models/Activity.js
+++ server/src/models/Activity.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../db.js'; 
+import sequelize from '../db.js';
 const Activity = sequelize.define("Activity", {
     title: {
         type: DataTypes.STRING,
